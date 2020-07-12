2f8aa11d0e3aa97abcdbaeb4d738c91dae9f603d
Parthi J <parthij15@gmail.com>
2020-07-13 00:04:39 +0530

    Build Files

---
+++ precache-manifest.665ccbd98b6800329ac5bcfb0cb3fa95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d7ea1b03cf4b78b7584754ffef8ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e0b5552bd9082624576",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94e1992061520c2517fe",
+    "url": "/static/js/2.5c948102.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5c948102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0b5552bd9082624576",
+    "url": "/static/js/main.2fca02c7.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "9a93f99cc6b6a143a328b8565c1247a5",
+    "url": "/static/media/background.9a93f99c.png"
+  },
+  {
+    "revision": "ed744619992427bec260dc44643699b8",
+    "url": "/static/media/profile.ed744619.jpg"
+  }
+]);
